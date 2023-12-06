939a0cceb160de52a2e7ba194bf1cdb2c6657b14
Audrey Dutcher <audrey@rhelmot.io>
2023-12-06 12:42:22 -0700

    Release 1.1.0

--- sound/__init__.py
+++ sound/__init__.py
@@ -5,8 +5,8 @@ sd.default.samplerate = SAMPLE_RATE  # type: ignore
 sd.default.channels = 1  # type: ignore
 
 __all__ = ('tone', 'sample', 'envelope', 'filter', 'instrument', 'notes', 'note', 'asyncplayer')
-__version__ = "1.0.1-dev.1"
-released_version = "1.0.0"
+__version__ = "1.1.1-dev.1"
+released_version = "1.1.0"
 
 from . import tone, sample, envelope, filter, instrument, notes, note, asyncplayer
 
